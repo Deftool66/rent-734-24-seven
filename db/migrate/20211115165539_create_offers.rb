a9e553f90737d7681ee9430901ae19737a685b65
Tarik Azale <deftool@gmx.net>
2021-11-15 18:02:12 +0100

    Add Offer Model

---
+++ db/migrate/20211115165539_create_offers.rb
@@ -0,0 +1,14 @@
+class CreateOffers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :offers do |t|
+      t.string :title
+      t.text :description
+      t.string :category
+      t.datetime :start_time
+      t.datetime :end_time
+      t.references :users, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
