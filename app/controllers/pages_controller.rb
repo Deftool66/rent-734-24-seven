a7b3dad31658eecded7a5e13d6d309a0178bc2fc
Leon Langhoff <leon.langhoff@posteo.de>
2021-11-17 00:19:47 +0100

    did a little frontend session

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,8 @@ class PagesController < ApplicationController
     skip_before_action :authenticate_user!, only: :home
   def home
   end
+
+  def random_user
+    @user = User.order(Arel.sql('RANDOM()')).first
+  end
 end
