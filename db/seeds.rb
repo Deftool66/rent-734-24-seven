3922ee19f4ef71c560a6d1d9cce13ee932749ca9
Leon Langhoff <leon.langhoff@posteo.de>
2021-11-16 15:25:55 +0100

    added seed for offers

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,10 +7,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-require 'faker'
+
 puts "clearning db"
+
+Offer.delete_all
 User.delete_all
+
 puts 'Creating 28 fake users...'
+
 28.times do
   user = User.new(
     password: "123456",
@@ -21,5 +26,21 @@ puts 'Creating 28 fake users...'
     skillset: "Ruby, HTML, CSS, js"
   )
   user.save!
+end
+
+puts "adding 100 offers"
+
+100.times do
+  offer = Offer.new(
+    title: Faker::App.name,
+    description: Faker::TvShows::HowIMetYourMother.quote,
+    category: ["js", "CSS", "HTML", "Ruby"].sample,
+    start_time: Faker::Time.backward(days: 5, period: :morning, format: :short),
+    end_time: Faker::Time.forward(days: 5,  period: :evening, format: :long),
+    user: User.all.sample
+
+    # rand(1..28)
+  )
+  offer.save!
 end
 puts 'Finished!'
