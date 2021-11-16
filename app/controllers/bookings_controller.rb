e7d332b701db0f35ce3e58e664bfe1f4f46029f6
Jesse Danoesastro <jessedanoe18@gmail.com>
2021-11-16 14:29:41 +0100

    Working on the booking mvc

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,35 @@
+class BookingsController < ApplicationController
+  before_action :set_params, only: [:show]
+  def index
+    @bookings = Booking.all
+  end
+
+  def show
+  end
+
+  def new
+    @booking = Booking.new
+    @offer = Offer.find(params[:offer_id])
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @offer = Offer.find(params[:offer_id])
+    @booking.offer = @offer
+    if @booking.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_params
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:start_time, :end_time)
+  end
+end
