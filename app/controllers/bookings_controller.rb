f59c1090b0311510b3defa3844e3994f7b61ecd3
Jesse Danoesastro <jessedanoe18@gmail.com>
2021-11-18 13:01:32 +0100

    made status for bookings and displayed them in the dashboard

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,9 +1,10 @@
 class BookingsController < ApplicationController
-  before_action :set_params, only: [:show]
-
+  before_action :set_params, only: [:show, :update]
 
   def index
     @bookings = Booking.all
+    @booking = Booking.find(params[:booking]) if params[:booking]
+    booking_status if params[:name]
   end
 
   def show
@@ -26,6 +27,19 @@ class BookingsController < ApplicationController
     end
   end
 
+  def booking_status
+    status = params[:name]
+    if status == 'accept'
+      @booking.status = "Accepted"
+    elsif status == 'decline'
+      @booking.status = 'Declined'
+    end
+    @booking.save
+  end
+
+  def update
+  end
+
   private
 
   def set_params
@@ -33,6 +47,6 @@ class BookingsController < ApplicationController
   end
 
   def booking_params
-    params.require(:booking).permit(:start_time, :end_time)
+    params.require(:booking).permit(:start_time, :end_time, :status)
   end
 end
